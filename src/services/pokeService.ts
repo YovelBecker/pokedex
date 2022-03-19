2e24aa56223c6e1968a6da375a2a2199a70d1e3d
YovelBecker <yovelb@misterbit.co.il>
2022-03-19 03:10:25 +0200

    added get Pokemon Data function for pokeService

--- src/services/pokeService.ts
+++ src/services/pokeService.ts
@@ -29,3 +29,68 @@ function getPokemonList(pokemon_entries: any) {
   });
   return pokemons
 }
+
+async function getPokemonData(pokemonsRaw: IPokemonRaw[]) {
+  const pokemonsDataPrms = pokemonsRaw.map(pokemon => {
+    const url = pokemon.url.slice(0, pokemon.url.length - 1)
+    const startIdx = url.lastIndexOf('/')
+    const pokedexId = +url.slice(startIdx + 1)
+    return httpService.get(`https://pokeapi.co/api/v2/pokemon/${pokedexId}`);
+  })
+  const pokemonsData = await Promise.all(pokemonsDataPrms)
+  const pokemons = pokemonsData.map((pokemonData: any) => {
+    const types = pokemonData.types.map(({ type }: any) => type);
+    const name = pokemonData.name;
+    const img = pokemonData.sprites.other['official-artwork'].front_default;
+    const weight = pokemonData.weight;
+    const stats = pokemonData.stats.map(({ base_stat: baseStat, effort, stat: { name, url } }: any) => {
+      return {
+        baseStat,
+        effort,
+        name,
+        url
+      }
+    })
+    const moves = pokemonData.moves.map(({ move, version_group_details }: any) => {
+      const versionGroupDetails = version_group_details.map((version: any) => {
+        return {
+          levelLearnedAt: version.level_learned_at,
+          moveLearnMethod: version.move_learn_method,
+          versionGroup: version.version_group
+        }
+      })
+      return {
+        ...move,
+        versionGroupDetails
+      }
+    });
+    const pokedexId = pokemonData.id;
+    const height = pokemonData.height;
+    const baseExp = pokemonData.base_experience;
+    const abilities = pokemonData.abilities.map(({ ability, is_hidden: isHidden, slot }: any) => {
+      return {
+        ability,
+        isHidden,
+        slot
+      }
+    });
+    return {
+      pokedexId,
+      name,
+      weight,
+      height,
+      img,
+      types,
+      baseExp,
+      abilities,
+      stats,
+      moves
+    }
+  })
+
+  return pokemons
+}
+
+const pokeService = { getData, getPokemonData }
+
+export default pokeService ;
