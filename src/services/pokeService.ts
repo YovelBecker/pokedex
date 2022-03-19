500c5f6078c518848b1236ec92e6ec3afc896912
YovelBecker <yovelb@misterbit.co.il>
2022-03-19 03:09:41 +0200

    added getData function for pokeService for init region data from pokeApi

--- src/services/pokeService.ts
+++ src/services/pokeService.ts
@@ -1,3 +1,31 @@
 import httpService from './httpService';
 import { Region } from '../models/Region';
 import { IPokemonRaw } from '../models/Pokemon';
+
+async function getData() {
+  const regs = (await (httpService.get('https://pokeapi.co/api/v2/generation'))).results;
+  const gensPrms = regs.map((gen: any) => {
+    return httpService.get(gen.url);
+  })
+  const gens = await Promise.all(gensPrms);
+  const regionsPrms = gens.map(gen => {
+    return httpService.get(gen.main_region.url);
+  })
+  const regionsRes = await Promise.all(regionsPrms);
+  const acc: any = { regions: [], pokemonsMap: {} }
+  for (let i = 0; i < regionsRes.length; i++) {
+    const regionData = regionsRes[i];
+    const { id, name } = regionData;
+    const pokedex = await httpService.get(regionData.pokedexes[0].url)
+    acc.regions.push(new Region(id, name))
+    acc.pokemonsMap[id] = getPokemonList(pokedex.pokemon_entries);
+  }
+  return acc
+}
+
+function getPokemonList(pokemon_entries: any) {
+  const pokemons = pokemon_entries.map((pokemon: any, idx: number) => {
+    return { pokedexId: idx, url: pokemon.pokemon_species.url }
+  });
+  return pokemons
+}
