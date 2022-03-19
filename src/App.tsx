f91da85ca60a48c8e4a3879f900d8388fb72d036
YovelBecker <yovelb@misterbit.co.il>
2022-03-19 03:10:54 +0200

    loading and setting data for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import Header from './components/Header';
+import pokeService from './services/pokeService'
 
 function App() {
+
+  const [dataMap, setDataMap] = useState<any>({});
+  const [currRegion, setCurrRegion] = useState<string | number>(2);
+
+  useEffect(() => {
+    const loadRegions = async () => {
+      const { regions, pokemonsMap } = await pokeService.getData();
+      setCurrRegion(Object.keys(pokemonsMap)[0])
+      setDataMap({ regions, pokemonsMap })
+    }
+    loadRegions();
+  }, [])
+
+  const changeRegion = (regionId: number | string) => {
+    if(regionId === currRegion) return
+    setCurrRegion(regionId)
+  }
+
   return (
     <div className="App">
       <Header />
