dc9c1671ba69d6dd75ca10bff9848357d3884ac2
YovelBecker <yovelb@misterbit.co.il>
2022-03-20 20:42:59 +0200

    added select region component

---
+++ src/components/Regions/SelectRegion.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { IPokemonItem } from '../../models/Pokemon'
+import { Region } from '../../models/Region'
+import pokeService from '../../services/pokeService'
+
+type StartersMap = {
+  [key: string]: IPokemonItem[]
+}
+
+export default function SelectRegion() {
+  const romans = ['I',
+    'II',
+    'III',
+    'VI',
+    'V',
+    'VI',
+    'VII',
+    'VIII']
+  const [startersMap, setStartersMap] = useState<StartersMap>({});
+  useEffect(() => {
+    const getStarters = async () => {
+      const startersMap = await pokeService.getStarters();
+      setStartersMap(startersMap);
+    }
+    getStarters()
+  }, [])
+  return (
+    <div className="select-region-container">
+      {Object.keys(startersMap).map((gen: string) => {
+        const starters = startersMap[gen];
+        return (
+          <Link key={gen} to={`/region/${gen}`} className="select-region-preview">
+            <img
+              className="pokeball"
+              src={require('../../assets/img/pokeball.png')}
+              alt="pokeball" />
+
+            <h4>Generation {romans[(+gen) - 1]}</h4>
+            <div className="sprites-container">
+              {starters.map(starter => (
+                <img key={starter.pokedexId} src={starter.img} alt={starter.name} className="sprite" />
+              ))}
+            </div>
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
