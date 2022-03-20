673ececc9088aa312bd271741664d7cf961e71fb
YovelBecker <yovelb@misterbit.co.il>
2022-03-20 20:43:27 +0200

    fixed header template with back button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Header() {
+
+  const navigate = useNavigate();
+
   return (
     <header>
+      <div className="header-nav">
+        <img onClick={() => navigate('/')} src={require('../assets/img/back_arrow.png')} alt="" />
+        <img src={require('../assets/img/menu.png')} alt="" />
+      </div>
+      <img
+        className="pokeball"
+        src={require('../assets/img/pokeball.png')}
+        alt="pokeball" />
       <h1>pokedex</h1>
     </header>
   )
