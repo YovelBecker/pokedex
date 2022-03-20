687e6aa7e2b58c3ea7220b3752bfd18b6f38907f
YovelBecker <yovelb@misterbit.co.il>
2022-03-20 20:44:20 +0200

    fixed pokemon list to use region id from url params

--- src/components/Pokemon/PokemonList.tsx
+++ src/components/Pokemon/PokemonList.tsx
@@ -1,30 +1,46 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
 import { IPokemonItem, IPokemonRaw } from '../../models/Pokemon'
 import pokemonService from '../../services/pokeService';
 import PokemonPreview from './PokemonPreview';
 
-type PokemonListProps = {
-  pokemonsRaw: IPokemonRaw[],
+type RouteParams = {
+  regionId: string
 }
 
-export default function PokemonList({ pokemonsRaw }: PokemonListProps) {
+export default function PokemonList() {
 
+  const { regionId } = useParams<RouteParams>();
+  const navigate = useNavigate();
   const [pokemons, setPokemons] = useState<IPokemonItem[]>([]);
 
   useEffect(() => {
+    if (!regionId) return
     const getPokemons = async () => {
+      setPokemons([])
+      const pokemonsRaw: IPokemonRaw[] = pokemonService.getPokemons(regionId);
+      if (!pokemonsRaw || !pokemonsRaw.length) {
+        navigate('/')
+        return
+      }
       const pokemons: IPokemonItem[] = await pokemonService.getPokemonData(pokemonsRaw);
       setPokemons(pokemons)
     }
     getPokemons();
-  }, [pokemonsRaw])
+    return () => { }
+  }, [regionId])
 
 
   return (
-    <div className='pokemon-list-container'>
-      {pokemons.length && pokemons.map((pokemon: IPokemonItem, idx: number) => (
-        <PokemonPreview idx={idx + 1} key={pokemon.pokedexId} pokemon={pokemon} />
-      ))}
-    </div>
+    <>
+      {!pokemons.length ?
+        <img className="pokeball loader" src={require('../../assets/img/pokeball.png')} alt="pokeball" /> :
+        <div className='pokemon-list-container'>
+          {pokemons.map((pokemon: IPokemonItem, idx: number) => {
+            return (<PokemonPreview idx={idx + 1} key={pokemon.pokedexId} pokemon={pokemon} />)
+          })}
+        </div>
+      }
+    </>
   )
 }
