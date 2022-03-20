bc533ad2a0a14109cd74c1e0ab74928914ead26b
YovelBecker <yovelb@misterbit.co.il>
2022-03-20 20:44:29 +0200

    added routing to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
-import PokemonList from './components/Pokemon/PokemonList';
 import RegionList from './components/Regions/RegionList';
+import SelectRegion from './components/Regions/SelectRegion';
+import { Region } from './models/Region';
 import pokeService from './services/pokeService'
 
 function App() {
 
-  const [dataMap, setDataMap] = useState<any>({});
-  const [currRegion, setCurrRegion] = useState<string | number>(2);
+  const [regions, setRegions] = useState<Region[]>([]);
+  const [currRegion, setCurrRegion] = useState<string | number>(0);
 
   useEffect(() => {
     const loadRegions = async () => {
       const { regions, pokemonsMap } = await pokeService.getData();
       setCurrRegion(Object.keys(pokemonsMap)[0])
-      setDataMap({ regions, pokemonsMap })
+      setRegions( regions)
     }
     loadRegions();
   }, [])
 
   const changeRegion = (regionId: number | string) => {
-    if(regionId === currRegion) return
+    if (regionId === currRegion) return
     setCurrRegion(regionId)
   }
 
   return (
     <div className="app">
       <Header />
-      {dataMap.regions && <RegionList currRegion={currRegion} changeRegion={changeRegion} regions={dataMap.regions} />}
-      {dataMap.pokemonsMap && <PokemonList pokemonsRaw={dataMap.pokemonsMap[currRegion]} />}
+      {regions &&
+        <RegionList
+          currRegion={currRegion}
+          changeRegion={changeRegion}
+          regions={regions} />
+      }
+      <Outlet />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/main.scss';
 import App from './App';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PokemonList from './components/Pokemon/PokemonList';
+import Home from './views/Home';
+import SelectRegion from './components/Regions/SelectRegion';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/region" element={<App />} >
+        <Route path="" element={<SelectRegion />} />
+        <Route path=":regionId" element={<PokemonList />} />
+      </Route>
+    </Routes>
+  </Router>,
   document.getElementById('root')
 );
